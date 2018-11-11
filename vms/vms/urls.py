89b7031615a417178e3898ce0dacc6e8e8a2d7ef
Tlazypanda <mishrasneha.bb@gmail.com>
2018-11-11 08:16:31 +0530

    Revert change on other branch

--- vms/vms/urls.py
+++ vms/vms/urls.py
@@ -12,6 +12,7 @@ urlpatterns = i18n_patterns(
     url(r'^authentication/',
         include('authentication.urls', namespace='authentication')),
     url(r'^event/', include('event.urls', namespace='event')),
+    url(r'^home/', include('home.urls', namespace='home')),
     url(r'^job/', include('job.urls', namespace='job')),
     url(r'^organization/',
         include('organization.urls', namespace='organization')),
